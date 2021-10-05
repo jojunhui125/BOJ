5d4b100f3f181371eb957fd1e4450a2d4a6f7bc3
junhee <86397805+jojunhui125@users.noreply.github.com>
2021-10-05 14:55:57 +0900

    BOJ 2751

---
+++ class2/2751.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int arr[1000001];
+int main(void)
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	sort(arr,arr+n);
+	
+	for(int i=0;i<n;i++)
+	{
+		printf("%d\n",arr[i]);
+	}
+	return 0;
+}
